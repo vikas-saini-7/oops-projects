56603cf7fb41e4b2d6ef1b24ce22fb7ce88c21e6
vikas-saini-7 <vikas289888@gmail.com>
2026-01-11 15:49:38 +0530

    feat: basic hashmap design

---
+++ hashmap_design.cpp
@@ -0,0 +1,35 @@
+// HASHMAP using C++
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class HashMap
+{
+private:
+    // storing data in table
+    vector<pair<int, string>> table;
+
+    // constructors
+    HashMap()
+    {
+        table;
+    }
+    HashMap(vector<pair<int, string>> data)
+    {
+        table = data;
+    }
+
+    // methods
+    void push(pair<int, string> data)
+    {
+        table.push_back(data);
+    }
+};
+
+int main()
+{
+    HashMap mpp();
+
+    mpp.push({2, "vikas"});
+    return 0;
+}
